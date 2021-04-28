503214ed2e6195cd906885fc0ed27cba435c4114
mj2360 <56599862+mj2360@users.noreply.github.com>
2021-04-28 13:26:04 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-  
-module.exports = {
-    consumer_key:         'a5aF4gexrdjhG7GrCcyOuiMEF',
-    consumer_secret:      'iY8yOKWauZRDVZA8QRdzol8Nnr9J8QmiNahYo76AXq7gIgsRQo',
-    access_token:         '1386386811644268554-TNKHWPdCvyRXvPCwcuQdDKRgJHjFf1',
-    access_token_secret:  'lhVIWyUTTbTnR9riOmsNyxVNRXBLxcMWHRuckeUuEdm7h',
-  }
