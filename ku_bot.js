90b3395b065067a708752606060eecf4dc24a21f
mj2360 <mj2360@nyu.edu>
2021-04-28 12:22:29 -0400

    added grammar rules json

--- ku_bot.js
+++ ku_bot.js
@@ -5,5 +5,17 @@ const fs = require('fs');
 
 
 config = require ("./config.js")
+rules = require("./rules.json");
 
 var T = new Twit (config);
+
+var grammar = Rita.grammar(rules); 
+console.log(grammar.expand());
+
+// var rules = {
+//     start: "$subject $verb $object.",
+//     subject: "I | You | They",
+//     object:  "coffee | bread | milk", 
+//     verb: "want | hate | like | love"
+// }
+
