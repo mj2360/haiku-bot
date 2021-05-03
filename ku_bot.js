903942fc436890ec509133668832bebeb14eefac
mj2360 <mj2360@nyu.edu>
2021-05-03 11:45:20 -0400

    successfully pushed verbs and syllables to JSON

--- ku_bot.js
+++ ku_bot.js
@@ -1,14 +1,19 @@
-const Twit = require('twit'); 
 const Rita = require('rita'); 
 const request = require('request'); 
 const fs = require('fs'); 
 
 
-config = require ("./config.js")
-//creating JSON file for RiGrammar rules
-rules = require("./rules.json");
+var noun_syll = {
+    1: ['spring', 'fall', 'death', 'trees', 'wind', 'leaves', 'leaf', 'sprout', 'time', 'cave', 'pine', 'oak', 'rot', 'earth', 'eye', 'grief', 'light', 'hell', 'cliff', 'hill', 'will', 'bird'], 
+    2: ['autumn', 'summer', 'winter', 'texture', 'newborn', 'repose', 'flower', 'petal', 'sunlight', 'moonlight', 'moonbeam', 'night sky', 'human', 'spiral', 'planet', 'orbit', 'feline', 'body', 'lion', 'tiger', 'beauty', 'darkness', 'shadow', 'lightness', 'goddess'], 
+    3: ['mystery', 'harmony', 'memory', 'masculine', 'feminine', 'melody', 'existence', 'universe', 'infinte', 'violence', 'medicine', 'paradise', 'dopamine', 'innocence'], 
+    4: ['dissolution', 'mortality', 'morality', 'phenomenon', 'melancholy', 'serenity', 'lonliness', 'silhouette', 'relationship', 'vegetable', 'homecoming', 'integrity', 'constellation', 'organism', 'ecosystem', 'territory', 'environment', 'destination', 'perimeter', 'bacteria', 'ressurection', 'awakening', 'biology', 'experiment', 'defloration', 'society']
+}
 
-var T = new Twit (config);
+let wordData = JSON.stringify(noun_syll, null, 2);
+fs.writeFileSync('words.json', wordData);
+
+
 
 var page = "1"; 
 var verbData; 
@@ -17,9 +22,11 @@ var aVerb = "";
 var verb_syll = {}; 
 var syllablesCount; 
 
+
 //searching for words in API that match POS parameter
 //returns all the verbs
-for (page = 1; page<2; page++){
+//page < 1150
+for (page = 1; page<4; page++){
     const options = {
         method: 'GET',
         url: 'https://wordsapiv1.p.rapidapi.com/words/?partOfSpeech=verb&page='+ page,
@@ -33,20 +40,17 @@ for (page = 1; page<2; page++){
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
         verbData  = JSON.parse(body);
-        //  console.log(verbData); 
-    
 
-        //pushing word data into verbs array 
+        //pushing data from WordsAPI into verbs array 
         for(i=0; i<verbData.results.data.length; i++) {
             verbs.push(verbData.results.data[i]); 
             
         } 
 
-
-
         //for each word in the verbs array pass it through Rita.analyze to get the syllable count for each word
         for(w=0; w<verbs.length; w++){
             aVerb = verbs[w]; 
+            
            
             var features = Rita.analyze(aVerb);
             // Just look at the syllables
@@ -54,34 +58,42 @@ for (page = 1; page<2; page++){
             // What's the count of syllables based on the slashes
             syllablesCount = syllables.split(/\//).length;
 
-            //Verbs by Syllable Object 
-            //key = syllablecount 
-            //value = [v,v,v,...]
-            verb_syll ={
-                1: [], 
-                2: [], 
-                3: ['airbrush'], 
-                4: []
-            }
             //pushing word to verb_syll Object based on syllable count
-                //Needs Fixing: only adds the last word from aVerb array
+            //Needs Fixing: only adds the last word from aVerb array
             if(syllablesCount in verb_syll){
-                verb_syll[syllablesCount].push(verbs[w]);
-            }
-        }
-        console.log(verb_syll);
-    });
+                verb_syll[syllablesCount].push(aVerb);
+                console.log(verb_syll[1]);
+                console.log(verb_syll[2]);
+            } else {
+                verb_syll[syllablesCount] = [];
+                verb_syll[syllablesCount].push(aVerb);
+            } 
+        } 
+        //   let wordData2 = JSON.stringify(verb_syll, null, 2);
+        //   fs.appendFileSync('words.json', wordData2);
+        
+    }); 
 }
 
 
 
 
-var noun_syll = {
-    1: ['spring', 'fall', 'death', 'trees', 'wind', 'leaves', 'leaf', 'sprout', 'time', 'cave', 'pine', 'oak', 'rot', 'earth', 'eye', 'grief', 'light', 'hell', 'cliff', 'hill', 'will', 'bird'], 
-    2: ['autumn', 'summer', 'winter', 'texture', 'newborn', 'repose', 'flower', 'petal', 'sunlight', 'moonlight', 'moonbeam', 'night sky', 'human', 'spiral', 'planet', 'orbit', 'feline', 'body', 'lion', 'tiger', 'beauty', 'darkness', 'shadow', 'lightness', 'goddess'], 
-    3: ['mystery', 'harmony', 'memory', 'masculine', 'feminine', 'melody', 'existence', 'universe', 'infinte', 'violence', 'medicine', 'paradise', 'dopamine', 'innocence'], 
-    4: ['dissolution', 'mortality', 'morality', 'phenomenon', 'melancholy', 'serenity', 'lonliness', 'silhouette', 'relationship', 'vegetable', 'homecoming', 'integrity', 'constellation', 'organism', 'ecosystem', 'territory', 'environment', 'destination', 'perimeter', 'bacteria', 'ressurection', 'awakening', 'biology', 'experiment', 'defloration', 'society']
-}
+
+
+
+
+
+
+
+
+
+//Demo 
+//a way to call a random one syllable word 
+var syll1 = noun_syll[1];
+var rando_syll1 = Math.floor(Math.random()*syll1.length);
+// console.log(rando_syll1);
+// console.log(noun_syll[1][rando_syll1]);
+
 
 
 
