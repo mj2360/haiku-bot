337ef0878740eb40be718e3eb166b6763f87c10c
mj2360 <mj2360@nyu.edu>
2021-05-05 14:03:49 -0400

    made p5 sketch to test haiku lines

--- ku_bot.js
+++ ku_bot.js
@@ -2,31 +2,36 @@ const Rita = require('rita');
 const request = require('request'); 
 const fs = require('fs'); 
 
-
-var noun_syll = {
-    1: ['spring', 'fall', 'death', 'trees', 'wind', 'leaves', 'leaf', 'sprout', 'time', 'cave', 'pine', 'oak', 'rot', 'earth', 'eye', 'grief', 'light', 'hell', 'cliff', 'hill', 'will', 'bird'], 
-    2: ['autumn', 'summer', 'winter', 'texture', 'newborn', 'repose', 'flower', 'petal', 'sunlight', 'moonlight', 'moonbeam', 'night sky', 'human', 'spiral', 'planet', 'orbit', 'feline', 'body', 'lion', 'tiger', 'beauty', 'darkness', 'shadow', 'lightness', 'goddess'], 
-    3: ['mystery', 'harmony', 'memory', 'masculine', 'feminine', 'melody', 'existence', 'universe', 'infinte', 'violence', 'medicine', 'paradise', 'dopamine', 'innocence'], 
-    4: ['dissolution', 'mortality', 'morality', 'phenomenon', 'melancholy', 'serenity', 'lonliness', 'silhouette', 'relationship', 'vegetable', 'homecoming', 'integrity', 'constellation', 'organism', 'ecosystem', 'territory', 'environment', 'destination', 'perimeter', 'bacteria', 'ressurection', 'awakening', 'biology', 'experiment', 'defloration', 'society']
-}
-
-let wordData = JSON.stringify(noun_syll, null, 2);
-fs.writeFileSync('words.json', wordData);
-
-
-
 var page = "1"; 
 var verbData; 
 var verbs = []; 
 var aVerb = "";
 var verb_syll = {}; 
+var noun_syll = {}; 
 var syllablesCount; 
+var wordObj = {};
+
+writeData();
+function writeData(){
+    wordObj = {
+        noun_syll : {
+            1: ['spring', 'fall', 'death', 'trees', 'wind', 'leaves', 'leaf', 'sprout', 'time', 'cave', 'pine', 'oak', 'rot', 'earth', 'eye', 'grief', 'light', 'hell', 'cliff', 'hill', 'will', 'bird'], 
+            2: ['autumn', 'summer', 'winter', 'texture', 'newborn', 'repose', 'flower', 'petal', 'sunlight', 'moonlight', 'moonbeam', 'night sky', 'human', 'spiral', 'planet', 'orbit', 'feline', 'body', 'lion', 'tiger', 'beauty', 'darkness', 'shadow', 'lightness', 'goddess'], 
+            3: ['mystery', 'harmony', 'memory', 'masculine', 'feminine', 'melody', 'existence', 'universe', 'infinte', 'violence', 'medicine', 'paradise', 'dopamine', 'innocence'], 
+            4: ['dissolution', 'mortality', 'morality', 'phenomenon', 'melancholy', 'serenity', 'lonliness', 'silhouette', 'relationship', 'vegetable', 'homecoming', 'integrity', 'constellation', 'organism', 'ecosystem', 'territory', 'environment', 'destination', 'perimeter', 'bacteria', 'ressurection', 'awakening', 'biology', 'experiment', 'defloration', 'society']
+        },
+
+        verb_syll : {
+            1: [],
+        }
+    }
+}
+
 
 
 //searching for words in API that match POS parameter
 //returns all the verbs
-//page < 1150
-for (page = 1; page<4; page++){
+for (page = 1; page<114; page++){
     const options = {
         method: 'GET',
         url: 'https://wordsapiv1.p.rapidapi.com/words/?partOfSpeech=verb&page='+ page,
@@ -43,38 +48,42 @@ for (page = 1; page<4; page++){
 
         //pushing data from WordsAPI into verbs array 
         for(i=0; i<verbData.results.data.length; i++) {
-            verbs.push(verbData.results.data[i]); 
+            //if word is already in verbs (array) do nothing
+            //else push 
+            if(verbData.results.data[i] in verbs){
+
+            } else {
+                verbs.push(verbData.results.data[i]); 
+            }
             
         } 
 
         //for each word in the verbs array pass it through Rita.analyze to get the syllable count for each word
         for(w=0; w<verbs.length; w++){
             aVerb = verbs[w]; 
-            
            
             var features = Rita.analyze(aVerb);
             // Just look at the syllables
             var syllables = features.syllables;
             // What's the count of syllables based on the slashes
-            syllablesCount = syllables.split(/\//).length;
+            syllablesCount = syllables.split(/\//).length.toString();
 
             //pushing word to verb_syll Object based on syllable count
-            //Needs Fixing: only adds the last word from aVerb array
-            if(syllablesCount in verb_syll){
-                verb_syll[syllablesCount].push(aVerb);
-                console.log(verb_syll[1]);
-                console.log(verb_syll[2]);
+            if(syllablesCount in wordObj.verb_syll){
+                wordObj.verb_syll[syllablesCount].push(aVerb);
             } else {
-                verb_syll[syllablesCount] = [];
-                verb_syll[syllablesCount].push(aVerb);
+                wordObj.verb_syll[syllablesCount] = [];
+                wordObj.verb_syll[syllablesCount].push(aVerb);
             } 
         } 
-        //   let wordData2 = JSON.stringify(verb_syll, null, 2);
-        //   fs.appendFileSync('words.json', wordData2);
-        
+        // console.log(verbs)
+        // console.log(wordObj);
+        // let wordData = JSON.stringify(wordObj, null, 2);
+        // fs.writeFileSync('words.json', wordData);
     }); 
 }
 
+console.log(verbs); 
 
 
 
@@ -87,12 +96,17 @@ for (page = 1; page<4; page++){
 
 
 
-//Demo 
-//a way to call a random one syllable word 
-var syll1 = noun_syll[1];
-var rando_syll1 = Math.floor(Math.random()*syll1.length);
-// console.log(rando_syll1);
-// console.log(noun_syll[1][rando_syll1]);
+
+
+
+
+
+// //Demo 
+// //a way to call a random one syllable word 
+// var syll1 = noun_syll[1];
+// var rando_syll1 = Math.floor(Math.random()*syll1.length);
+// // console.log(rando_syll1);
+// // console.log(noun_syll[1][rando_syll1]);
 
 
 
--- sketch.js
+++ sketch.js
@@ -1,35 +1,62 @@
-var lexicon;
+var wordData; 
+var lines = [[], [], []]; 
+var preps = ["for", "since", "within", "until", "before"];
+var randPrep; 
+var randLine = 0; 
+var randNoun1; 
+var randNoun2; 
+var randNoun3; 
+var randNoun4; 
+var randVerb1; 
+var randVerb2; 
+var randVerb3; 
+var randVerb4; 
+
+
+
+function preload (){
+    wordData = loadJSON('words.json'); 
+}
 
 function setup(){
-    createCanvas(500, 500); 
-    background(220); 
-   console.log(RiTa.randomWord({ targetPos: "", numSyllables: 4})); 
+    createCanvas(650, 200); 
 
-    // lexicon = new RiLexicon();
-    // fill(255);
-    // noStroke();
-    // textSize(24);
-    // textAlign(CENTER, CENTER);
-    // text("Click for haiku", width/2, height/2);
+    lines = [["click to", "generate", "a haiku"], ["", "", ""], []];
+
 }
 
 function draw(){
-
+    background(220); 
+    text(lines[randLine][0], width / 2, 75);
+    text(lines[randLine][1], width / 2, 110);
+    text(lines[randLine][2], width / 2, 145);
 }
-// function mousePressed() {
-//     background(50);
-//     var firstLine  = "the " + 
-//       lexicon.randomWord("jj", 2) + " " +
-//       lexicon.randomWord("nn", 2);
-//     var secondLine = lexicon.randomWord("vbg", 2) +
-//       " in the " +
-//       lexicon.randomWord("jj", 2) + " " +
-//       lexicon.randomWord("nn", 1);
-//     var thirdLine = "I " +
-//       lexicon.randomWord("vbd", 2) + " " + 
-//       lexicon.randomWord("rb", 2);
-//     text(firstLine, width/2, 150);
-//     text(secondLine, width/2, 200);
-//     text(thirdLine, width/2, 250);
-//   }
+
+
+function mouseReleased() {
+    randPrep = int (random(0, preps.length)); 
+    randLine = int(random(0, 2)); 
+    console.log(randLine);
+
+    randNoun2 = wordData.noun_syll[2][int(random(0,wordData.noun_syll[2].length -1))];
+    randNoun3 = wordData.noun_syll[3][int(random(0,wordData.noun_syll[3].length -1))]; 
+    randVerb1 = wordData.verb_syll[1][int(random(0,wordData.verb_syll[1].length -1))]; 
+    randVerb2 =  wordData.verb_syll[2][int(random(0,wordData.verb_syll[2].length -1))]
+    randVerb3 =  wordData.verb_syll[3][int(random(0,wordData.verb_syll[3].length -1))]; 
+
+
+    lines[0][0] = preps[randPrep] + " the " + randNoun3; 
+    lines[0][1] = "I " + randVerb2 + " to " + wordData.noun_syll[3][int(random(0,wordData.noun_syll[3].length -1))]; 
+    lines[0][2] = "But " + randVerb1 + " for " +  wordData.noun_syll[2][int(random(0,wordData.noun_syll[2].length -1))];
+    // console.log(lines[0][0], lines[0][1], lines[0][2]);
+    
+    lines[1][0] = "Even with the " + randNoun2;
+    lines[1][1] = "I feel the weight of " + wordData.noun_syll[2][int(random(0,wordData.noun_syll[2].length -1))];
+    lines[1][2] = "Taking time to " + randVerb1;
+
+    // line[2][0] = 
+    // line[2][1] =
+    // line[2][2] =
+  }
+
 
