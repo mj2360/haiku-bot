97a61f3586a8c3344ec523df56a808840da6de89
mj2360 <mj2360@nyu.edu>
2021-05-10 12:04:31 -0500

    added Procfile and config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    consumer_key:         'UtJ3t5sOeq5CaPTkGOUHVMHbI',
+    consumer_secret:      'wkf7FIAuko8RwEXvzH2RaNAfkqQHOeoQebm3znAfUzBbRCh2Fa',
+    access_token:         '1386386811644268554-9PYuBVNNXbVcHCY6QgSvFKO3rPZPl0',
+    access_token_secret:  'RACOlizspRKEc5bv3gJzJPxX2rCJV0YztRWrwZnHScLKC',
+  }
